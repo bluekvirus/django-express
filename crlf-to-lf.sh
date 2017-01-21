12b441fd3997d49e9220e0a99559b8ece59e953a
Tim Lauv <bluekvirus@gmail.com>
2017-01-21 07:51:01 +0000

    Fixed crlf -> lf in source and scripts;

--- crlf-to-lf.sh
+++ crlf-to-lf.sh
@@ -3,9 +3,16 @@
 # If you see this error msg:
 # bash: ./create-venv.sh: /bin/bash^M: bad interpreter: No such file or directory
 # 
+# Prevent:
 # core.autocrlf true (Windows <--> repo)
 # core.autocrlf input (Linux <--> repo)
+#
+# Fix: (.gitattributes)
+# *.sh        eol=lf
+# *.py        eol=lf
+#
+# Create the .gitattributes file and then run the following commands:
 
 git config --global core.autocrlf input
 git rm --cached -r .
-git reset --hard
+git reset --hard # run this line out of this file
