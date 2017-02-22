85dfff24735f850687a38b8555ac2ff3c9f13e19
mr-beaver <beavercoding@gmail.com>
2017-02-21 17:21:07 -0800

    modified Model.get() method; now it can take parameters ?filter=foo1:bar1&filter=foo2:bar2&sort=foo,bar&field=foo&page=2&size=3&offset=1;

--- testa/models.py
+++ testa/models.py
@@ -9,5 +9,6 @@ from express.decorators import serve, serve_unprotected, url
 class Device(models.Model):
 	"""docstring for Device"""
 	sn = models.CharField(max_length=32)
-
-		
+	#print self, for easier debugging
+	def __str__(self):
+		return str(self.id) + ' ' + str(self.sn)
