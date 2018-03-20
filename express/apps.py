2413f29a26d078680fd044a61878ea9eca6de604
ch3riee <cheriemhuang@gmail.com>
2018-03-19 18:22:05 -0700

    Fixed compatibility with django 2.0 (#6)
    * Fixed compatibility with django 2.0
    
    * Added backwards compatibility for django versions prior to 2.0. Specifically for 1.10 and 1.11
    
    * Adding some panic message and changing detection to startswith
    
    * Tidy things up

--- express/apps.py
+++ express/apps.py
@@ -1,16 +1,25 @@
 from django.apps import AppConfig
 import logging
+import django
 
 logger = logging.getLogger('django')
 
 
 class ExpressConfig(AppConfig):
-    name = 'express'
+	name = 'express'
 
-    def ready(self):
+	def ready(self):
         # auto load apis indicated by services.py, models.py from each (installed) app
-        self.module.autodiscover('services', 'models')
-        for url in self.module.services._generated:
-            logger.info('[express: uri] ' + str(url.regex.pattern) + ' -- (name: ' + str(url.name) + ')')
-        for root in self.module.services._global_urls:
-            logger.info('[django: base] ' + str(root.regex.pattern))
+	self.module.autodiscover('services', 'models')
+	if django.get_version().startswith('2'): # for version 2 of django
+		for url in self.module.services._generated:
+			logger.info('[express: uri] ' + str(url.pattern.regex.pattern) + ' -- (name: ' + str(url.name) + ')')
+		for root in self.module.services._global_urls:
+			logger.info('[django: base] ' + str(root.pattern.regex.pattern))
+	elif django.get_version().startswith('1'): # for versions 1.10, 1.11
+		for url in self.module.services._generated:
+			logger.info('[express: uri] ' + str(url.regex.pattern) + ' -- (name: ' + str(url.name) + ')')
+		for root in self.module.services._global_urls:
+			logger.info('[django: base] ' + str(root.regex.pattern))
+        else:
+		logger.error('Error, incompatible Django version detected!')
